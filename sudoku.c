562ef696c4b19eed5ba2f23c4b67ee9b695af741
flaviomax <flavio.maxi@gmail.com>
2014-03-06 15:45:05 -0300

    Nome da thread alterado

--- sudoku.c
+++ sudoku.c
@@ -6,7 +6,7 @@
 
 volatile int tabuleiro[9][9];
 
-void* f_thread(void *v) {
+void* f_thread_imprimir(void *v) {
   int i, j;
   for (i = 0; i < 9; i++){
     for (j = 0; j < 9; j++)
@@ -33,7 +33,7 @@ int main() {
 
   pthread_t thr;
   
-  if (pthread_create(&thr, NULL, f_thread, NULL))
+  if (pthread_create(&thr, NULL, f_thread_imprimir, NULL))
     fprintf(stderr, "Erro na criacao da thread. \n");
 
   pthread_join(thr, NULL);
